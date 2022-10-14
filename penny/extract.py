e8b04d7ed97207aa995ccf0c9a69fcd5848ca263
Tyler <ttylertran260@gmail.com>
2022-10-14 16:30:40 -0700

    Added function to return worksheet specified by parameters

---
+++ penny/extract.py
@@ -0,0 +1,16 @@
+# ==================================================
+# Imports
+import gspread as gs
+import json
+
+
+# ==================================================
+# Functions
+def get_data(sheet_id, ws_loc):
+    acc = gs.service_account()
+    sh = acc.open_by_key(sheet_id)
+    ws = sh.get_worksheet(ws_loc)
+    return ws 
+
+
+
