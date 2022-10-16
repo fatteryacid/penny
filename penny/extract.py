7157c04db250080139f4228e2daa620841743ada
Tyler <ttylertran260@gmail.com>
2022-10-16 08:56:57 -0700

    Revised get_data function to return records instead of object

--- penny/extract.py
+++ penny/extract.py
@@ -10,7 +10,7 @@ def get_data(sheet_id, ws_loc):
     acc = gs.service_account()
     sh = acc.open_by_key(sheet_id)
     ws = sh.get_worksheet(ws_loc)
-    return ws 
+    return ws.get_all_records()
 
 
 
