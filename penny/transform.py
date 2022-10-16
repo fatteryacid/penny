8a4829b7da8c7d01eb426618a5ce5d42de2cac83
Tyler <ttylertran260@gmail.com>
2022-10-16 08:56:27 -0700

    Added working dataframe parsing function

--- penny/transform.py
+++ penny/transform.py
@@ -8,12 +8,29 @@ import pandas pd
 # Functions
 def build_dataframe(worksheet, num_split, existing_tid):
     payload = pd.DataFrame(worksheet)
+    payload = payload.drop(' per person ', axis=1)
     tid = []
 
-    for i in payload.iterrows():
-        tid.append(str(i[0]) + str(i[1]) + str(i[5:5+int(num_split)]))  # tid = concat(date, item desc, split codes)
-
-    if existing_tid != None:    # return subsection if tid exists
+    for i in temp_df.itertuples(): #Build tid codes
+        temp = str(i[0])
+        temp += str(i[1]).replace('/', '')
+        temp += str(i[2]).replace(' ', '').lower()
+        temp += str(i[6])
+        temp += str(i[7])
+        temp += str(i[8])
+        
+        tid.append(temp)
+        
+    #Add new col to dataframe
+    payload['tid'] = tid
+    
+    #Check for save state
+    if existing_tid == None:
+        return payload
+    
+    else:
+        lower_bound = payload.loc[payload['tid'] == existing_tid].index[0]
+        return payload.iloc[lower_bound:, :]
         
 
 
