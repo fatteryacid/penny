e28980fa050920676a332fb11031a7a2174c80b0
Tyler <ttylertran260@gmail.com>
2022-10-17 08:57:04 -0700

    Created skeletal structure for load functions

---
+++ penny/load.py
@@ -0,0 +1,10 @@
+# ==================================================
+# Imports
+import json
+import urllib.parse
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+# ==================================================
+# Functions
