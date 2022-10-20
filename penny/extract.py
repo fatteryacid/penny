da9d3389e9851eefc89021fa69dfe70c249fd9be
Tyler <ttylertran260@gmail.com>
2022-10-20 10:58:37 -0700

    Added function to download backup upon each extract run

--- penny/extract.py
+++ penny/extract.py
@@ -1,16 +1,33 @@
 # ==================================================
 # Imports
+import pandas as pd
 import gspread as gs
-import json
+import datetime
+import csv
+
 
 
 # ==================================================
 # Functions
+def create_backup(ws):
+    #Example filename 01-01-1991_extract.csv
+    filename = '../log/' + datetime.datetime.now().strftime('%m-%d-%Y') + '_extract.csv'
+    
+    df = pd.DataFrame(ws.get_all_records())
+    df = df.replace('', np.nan)
+    df = df.dropna()
+    
+    df.to_csv(filename, index=False)
+
 def get_data(sheet_id, ws_loc):
+    #Connect to Google service account
     acc = gs.service_account()
     sh = acc.open_by_key(sheet_id)
     ws = sh.get_worksheet(ws_loc)
+
+    #Create backup
+    create_backup(ws)
+
+    #Return records
     return ws.get_all_records()
-
-
 
