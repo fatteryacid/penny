d1b20f9db78d523634cdf91518bfc4c05d66efe7
tyler <ttylertran260@gmail.com>
2022-10-20 09:07:38 -0700

    Update load.py

--- penny/load.py
+++ penny/load.py
@@ -1,10 +1,26 @@
 # ==================================================
 # Imports
-import json
-import urllib.parse
+import pandas pd 
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
 
 # ==================================================
 # Functions
+def check_vendors(df, engine):
+    df['store'].drop_duplicates(inplace=True)
+    
+
+    with engine.connect() as conn:
+        db_list = conn.execute(
+            text('SELECT DISTINCT vendor_desc FROM d_vendors')
+        )
+
+            
+
+
+def load_to_db(df, engine, behavior):
+    with engine.connect() as conn:
+        conn.execute(
+            text(f'INSERT INTO ')
+        )
