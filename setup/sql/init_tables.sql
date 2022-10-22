db2024d4293fd0252d4f2b416ab20baa4a7ce08c
Tyler <ttylertran260@gmail.com>
2022-10-22 12:36:26 -0700

    Revised DDL statement syntax

--- setup/sql/init_tables.sql
+++ setup/sql/init_tables.sql
@@ -3,22 +3,19 @@
 
 -- Category & Subcategory group
 CREATE TABLE d_category (
-    category_id SERIAL NOT NULL,
-    category_desc VARCHAR(50),
-    PRIMARY KEY category_id
+    category_id SERIAL NOT NULL PRIMARY KEY,
+    category_desc VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE d_subcategory (
-    subcategory_id SERIAL NOT NULL,
-    subcategory_desc VARCHAR(75),
-    PRIMARY KEY subcategory_id
+    subcategory_id SERIAL NOT NULL PRIMARY KEY,
+    subcategory_desc VARCHAR(75) UNIQUE
 );
 
 CREATE TABLE d_category_subcategory (
-    category_relation_id SERIAL NOT NULL,
+    category_relation_id SERIAL NOT NULL PRIMARY KEY,
     category_id INT NOT NULL,
     subcategory_id INT NOT NULL,
-    PRIMARY KEY category_relation_id,
     FOREIGN KEY category_id REFERENCES d_category(category_id),
     FOREIGN KEY subcategory_id REFERENCES d_subcategory(subcategory_id)
 );
@@ -26,21 +23,19 @@ CREATE TABLE d_category_subcategory (
 
 -- Vendor group
 CREATE TABLE d_vendor (
-    vendor_id SERIAL NOT NULL,
-    vendor_desc VARCHAR(100),
-    PRIMARY KEY vendor_id
+    vendor_id SERIAL NOT NULL PRIMARY KEY,
+    vendor_desc VARCHAR(100) UNIQUE
 );
 
 
 -- Fact table
 CREATE TABLE f_entry (
-    eid VARCHAR(36) NOT NULL,
+    eid VARCHAR(36) NOT NULL PRIMARY KEY,
     category_relation_id INT NOT NULL,
     vendor_id INT NOT NULL,
     amount DECIMAL,
     entry_record_date DATE,
     last_updated DATETIME,
-    PRIMARY KEY eid,
     FOREIGN KEY category_relation_id REFERENCES d_category_subcategory(category_relation_id),
     FOREIGN KEY vendor_id REFERENCES d_vendor(vendor_id)
 );
@@ -48,17 +43,15 @@ CREATE TABLE f_entry (
 
 -- Person group
 CREATE TABLE d_person (
-    person_id SERIAL NOT NULL,
+    person_id SERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    PRIMARY KEY person_id
 );
 
 CREATE TABLE d_person_distribution (
-    distribution_id BIGSERIAL NOT NULL,
+    distribution_id BIGSERIAL NOT NULL PRIMARY KEY,
     eid VARCHAR(36),
     person_id INT NOT NULL,
-    PRIMARY KEY distribution_id,
     FOREIGN KEY eid REFERENCES f_entry(eid),
     FOREIGN KEY person_id REFERENCES d_person(person_id)
 );
