7d07736b9d4b794af3f0ac21aa6a94ecadd32254
Tyler <ttylertran260@gmail.com>
2022-10-22 11:36:56 -0700

    Added scripts to create staging environment for testing

---
+++ tests/sql/stage_vendors.sql
@@ -0,0 +1,18 @@
+-- Run this script to initialize vendors table with data
+DROP TABLE d_vendor;
+
+CREATE TABLE d_vendor (
+    vendor_id SERIAL NOT NULL PRIMARY KEY,
+    vendor_desc VARCHAR(100)
+);
+
+INSERT INTO d_vendor (vendor_desc)
+VALUES
+    ('home_depot'),
+    ('lowes'),
+    ('microcenter'),
+    ('best_buy'),
+    ('toms_bikes'),
+    ('jennys_auto_body'),
+    ('cafe_boulard')
+;
