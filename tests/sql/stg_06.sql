f7261eb3a6cc81b0a5dfc93ce201b04723d1fa4e
Tyler <ttylertran260@gmail.com>
2022-10-24 13:38:45 -0700

    Create SQL script for entry and person join table

---
+++ tests/sql/stg_06.sql
@@ -0,0 +1,9 @@
+DROP TABLE d_person_distribution;
+
+CREATE TABLE d_person_distribution (
+    distribution_id SERIAL NOT NULL PRIMARY KEY,
+    eid CHAR(8) NOT NULL,
+    person_id INT NOT NULL,
+    CONSTRAINT fk_entry FOREIGN KEY (eid) REFERENCES f_entries(eid),
+    CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES d_person(person_id)
+);
