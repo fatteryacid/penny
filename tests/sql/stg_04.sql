421957ff0ad180f3769e11af46318bc14644fdc6
Tyler <ttylertran260@gmail.com>
2022-10-24 13:37:55 -0700

    Create SQL script for entry fact relation

---
+++ tests/sql/stg_04.sql
@@ -0,0 +1,15 @@
+DROP TABLE f_entries;
+
+CREATE TABLE f_entries (
+    eid CHAR(8) NOT NULL PRIMARY KEY,
+    category_id INT NOT NULL,
+    subcategory_id INT NOT NULL,
+    vendor_id INT NOT NULL,
+    amount DECIMAL(11,2),
+    entry_record_date DATE,
+    last_updated TIMESTAMP,
+    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id),
+    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id),
+    CONSTRAINT fk_vend FOREIGN KEY (vendor_id) REFERENCES d_vendor(vendor_id)
+);
+
