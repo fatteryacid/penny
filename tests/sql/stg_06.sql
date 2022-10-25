2b768f1d25011fac11dff07f6c79b216f2b24fbd
Tyler <ttylertran260@gmail.com>
2022-10-25 12:42:19 -0700

    Revised eid datatype to match updated character length

--- tests/sql/stg_06.sql
+++ tests/sql/stg_06.sql
@@ -2,7 +2,7 @@ DROP TABLE d_person_distribution;
 
 CREATE TABLE d_person_distribution (
     distribution_id SERIAL NOT NULL PRIMARY KEY,
-    eid CHAR(8) NOT NULL,
+    eid CHAR(17) NOT NULL,
     person_id INT NOT NULL,
     CONSTRAINT fk_entry FOREIGN KEY (eid) REFERENCES f_entries(eid),
     CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES d_person(person_id)
