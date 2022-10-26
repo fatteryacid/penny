cf46b9ecbc746d3ec6b05ec1d1a77a57379319de
tyler <ttylertran260@gmail.com>
2022-10-26 09:14:54 -0700

    Revised fact relation to use j_type ids

--- tests/sql/stg_05.sql
+++ tests/sql/stg_05.sql
@@ -3,14 +3,12 @@ DROP TABLE f_entries;
 CREATE TABLE f_entries (
     eid CHAR(17) NOT NULL PRIMARY KEY,
     item_desc VARCHAR(125),
-    category_id INT NOT NULL,
-    subcategory_id INT NOT NULL,
+    type_id INT NOT NULL
     vendor_id INT NOT NULL,
     amount DECIMAL(11,2),
     entry_record_date DATE,
     last_updated TIMESTAMP,
-    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id),
-    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id),
-    CONSTRAINT fk_vend FOREIGN KEY (vendor_id) REFERENCES d_vendor(vendor_id)
+    CONSTRAINT fk_type FOREIGN KEY (type_id) REFERENCES j_type(type_id) ON DELETE SET NULL,
+    CONSTRAINT fk_vend FOREIGN KEY (vendor_id) REFERENCES d_vendor(vendor_id) ON DELETE SET NULL
 );
 
