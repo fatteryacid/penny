4bad258874b41fd6d958f47f7886419acf9bbeb8
tyler <ttylertran260@gmail.com>
2022-10-26 09:04:55 -0700

    Offset naming conventions by 1 for join table script

--- tests/sql/stg_04.sql
+++ tests/sql/stg_04.sql
@@ -1,16 +1,18 @@
-DROP TABLE f_entries;
+-- Run this script to initialize vendors table with data
+DROP TABLE d_vendor;
 
-CREATE TABLE f_entries (
-    eid CHAR(17) NOT NULL PRIMARY KEY,
-    item_desc VARCHAR(125),
-    category_id INT NOT NULL,
-    subcategory_id INT NOT NULL,
-    vendor_id INT NOT NULL,
-    amount DECIMAL(11,2),
-    entry_record_date DATE,
-    last_updated TIMESTAMP,
-    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id),
-    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id),
-    CONSTRAINT fk_vend FOREIGN KEY (vendor_id) REFERENCES d_vendor(vendor_id)
+CREATE TABLE d_vendor (
+    vendor_id SERIAL NOT NULL PRIMARY KEY,
+    vendor_desc VARCHAR(100) UNIQUE
 );
 
+INSERT INTO d_vendor (vendor_desc)
+VALUES
+    ('home_depot'),
+    ('lowes'),
+    ('microcenter'),
+    ('best_buy'),
+    ('costco'),
+    ('ralphs'),
+    ('trader_joes')
+;
--- tests/sql/stg_05.sql
+++ tests/sql/stg_05.sql
@@ -1,9 +1,16 @@
-DROP TABLE d_person;
+DROP TABLE f_entries;
 
-CREATE TABLE d_person (
-    person_id SERIAL NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30)
+CREATE TABLE f_entries (
+    eid CHAR(17) NOT NULL PRIMARY KEY,
+    item_desc VARCHAR(125),
+    category_id INT NOT NULL,
+    subcategory_id INT NOT NULL,
+    vendor_id INT NOT NULL,
+    amount DECIMAL(11,2),
+    entry_record_date DATE,
+    last_updated TIMESTAMP,
+    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id),
+    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id),
+    CONSTRAINT fk_vend FOREIGN KEY (vendor_id) REFERENCES d_vendor(vendor_id)
 );
-
 
