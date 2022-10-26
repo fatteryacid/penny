c4cf447bd2d6903411014065af95452081916648
Tyler <ttylertran260@gmail.com>
2022-10-25 18:37:19 -0700

    Create shell script to build virtual environment if needed and execute if requirement is satisfied

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-source ./penny/venv/bin/activate
+if [ ! -d "./penny/venv" ]; then
+    cd ./penny/
+    python3 -m venv venv
+
+    source ./venv/bin/activate
+    pip install --upgrade pip 
+    pip install -r ../requirements.txt
+
+    cd ..
+
+else source ./penny/venv/bin/activate
+fi 
 
 cd ./setup/
 python3 setup.py 
