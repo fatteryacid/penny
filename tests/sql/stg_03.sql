5359f8561750770d9b1e46afa1167f04f0077f62
tyler <ttylertran260@gmail.com>
2022-10-26 09:13:00 -0700

    Added on delete behavior

--- tests/sql/stg_03.sql
+++ tests/sql/stg_03.sql
@@ -4,6 +4,6 @@ CREATE TABLE j_type (
     type_id SERIAL NOT NULL PRIMARY KEY,
     category_id INT NOT NULL,
     subcategory_id INT NOT NULL,
-    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id),
-    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id)
+    CONSTRAINT fk_cat FOREIGN KEY (category_id) REFERENCES d_category(category_id) ON DELETE SET NULL,
+    CONSTRAINT fk_sbcat FOREIGN KEY (subcategory_id) REFERENCES d_subcategory(subcategory_id) ON DELETE SET NULL
 );
--- tests/sql/stg_07.sql
+++ tests/sql/stg_07.sql
@@ -4,6 +4,6 @@ CREATE TABLE j_distribution (
     dist_id SERIAL NOT NULL PRIMARY KEY,
     eid CHAR(17) NOT NULL,
     person_id INT NOT NULL,
-    CONSTRAINT fk_entry FOREIGN KEY (eid) REFERENCES f_entries(eid),
-    CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES d_person(person_id)
+    CONSTRAINT fk_entry FOREIGN KEY (eid) REFERENCES f_entries(eid) ON DELETE SET NULL,
+    CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES d_person(person_id) ON DELETE SET NULL
 );
