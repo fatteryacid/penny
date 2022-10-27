61eb159a320b82c26741e017888cdd804dc07c05
Tyler <ttylertran260@gmail.com>
2022-10-27 13:56:24 -0700

    Refactor for configuration changes

--- penny/main.py
+++ penny/main.py
@@ -30,13 +30,23 @@ def table_to_dict(table, inverse=False):
 
 # ==================================================
 # Variables
-with open('./config.json') as f:
-    meta = json.load(f)
-    db = meta['database_information']
-    config = meta['config']
-    f.close()
+path = {
+    'db': './config/secret_db_conf.json',
+    'gs': './config/secret_gs_conf.json',
+    'sv': './config/secret_save.json'
+}
 
-engine_url = (f'postgresql+psycopg2://{db["db_user"]}:{urllib.parse.quote_plus(db["db_pass"])}@{db["db_host"]}:{db["db_port"]}/{db["db_name"]}')
+with open(path['db']) as f:
+    db = json.load(f)
+
+with open(path['gs']) as f:
+    gs = json.load(f)
+
+with open(path['sv']) as f:
+    sv = json.load(f)
+
+
+engine_url = (f'{db["creds"]["db_engine"]}://{db["creds"]["db_user"]}:{urllib.parse.quote_plus(db["creds"]["db_pass"])}@{db["creds"]["db_host"]}:{db["creds"]["db_port"]}/{db["creds"]["db_name"]}')
 
 
 
@@ -48,29 +58,29 @@ def main():
 
     #Retrieve data
     print('[PENNY] Retrieving data...')
-    data = ex.get_data(config['target_sheet'], config['worksheet_location'])
+    data = ex.get_data(gs['target_sheet'], gs['worksheet_location'])
     
     #Basic data cleanup
     print('[PENNY] Cleaning data..')
     data = tr.process_colname(data)
     data = tr.process_string(data, column_list=[
-        config['sheet_mapping']['id'],
-        config['sheet_mapping']['item'],
-        config['sheet_mapping']['category'],
-        config['sheet_mapping']['subcategory'],
-        config['sheet_mapping']['vendor']    
+        gs['sheet_mapping']['id'],
+        gs['sheet_mapping']['item'],
+        gs['sheet_mapping']['category'],
+        gs['sheet_mapping']['subcategory'],
+        gs['sheet_mapping']['vendor']    
     ])
-    data = tr.process_distribution(data, column_list=config['sheet_mapping']['distribution'])
+    data = tr.process_distribution(data, column_list=gs['sheet_mapping']['distribution'])
     data = tr.process_amount(data)
-    data = tr.process_ignore(data, config['sheet_mapping']['ignore'])
+    data = tr.process_ignore(data, gs['sheet_mapping']['ignore'])
 
     #Formatting
     print('[PENNY] Formatting data...')
-    data = tr.trunc_df(data, config['existing_id'])
+    data = tr.trunc_df(data, sv['existing_id'])
     data = tr.format_labels(data, column_list=[
-        config['sheet_mapping']['category'],
-        config['sheet_mapping']['subcategory'],
-        config['sheet_mapping']['vendor']
+        gs['sheet_mapping']['category'],
+        gs['sheet_mapping']['subcategory'],
+        gs['sheet_mapping']['vendor']
     ])
 
     #Control for no new updates
@@ -78,90 +88,58 @@ def main():
 
         #Save state
         last_eid = data.tail(1).iloc[0]['id']
-        meta['config']['existing_id'] = last_eid
+        sv['existing_id'] = last_eid
         new_state = json.dumps(meta, indent=4)
 
         if type(new_state) != str:
             raise Exception('[PENNY] FATAL ERROR: Writing to config failed.')
         else:
-            with open('./config.json', 'w') as out:
+            with open(path['sv'], 'w') as out:
                 out.write(new_state)
 
         #Create db table variables
-        vend = table(config['db_mapping']['vendor']['rel_name'],
-                column(config['db_mapping']['vendor']['id_col']),
-                column(config['db_mapping']['vendor']['desc_col'])
+        vend = table(db['schema']['vendor']['rel_name'],
+                column(db['schema']['vendor']['id_col']),
+                column(db['schema']['vendor']['desc_col'])
             )
         
-        cat = table(config['db_mapping']['category']['rel_name'],
-                column(config['db_mapping']['category']['id_col']),
-                column(config['db_mapping']['category']['desc_col'])
+        cat = table(db['schema']['category']['rel_name'],
+                column(db['schema']['category']['id_col']),
+                column(db['schema']['category']['desc_col'])
             )
 
-        sbcat = table(config['db_mapping']['subcategory']['rel_name'],
-                column(config['db_mapping']['subcategory']['id_col']),
-                column(config['db_mapping']['subcategory']['desc_col'])
+        sbcat = table(db['schema']['subcategory']['rel_name'],
+                column(db['schema']['subcategory']['id_col']),
+                column(db['schema']['subcategory']['desc_col'])
             )
 
-        j_type = table(config['db_mapping']['type']['rel_name'],
-                column(config['db_mapping']['type']['id_col']),
-                column(config['db_mapping']['type']['cat']),
-                column(config['db_mapping']['type']['sbcat'])
+        j_type = table(db['schema']['type']['rel_name'],
+                column(db['schema']['type']['id_col']),
+                column(db['schema']['type']['cat']),
+                column(db['schema']['type']['sbcat'])
             )
 
-        person = table(config['db_mapping']['person']['rel_name'],
-                column(config['db_mapping']['person']['id_col']),
-                column(config['db_mapping']['person']['fname']),
-                column(config['db_mapping']['person']['lname'])
+        person = table(db['schema']['person']['rel_name'],
+                column(db['schema']['person']['id_col']),
+                column(db['schema']['person']['fname']),
+                column(db['schema']['person']['lname'])
             )
 
-        distribution = table(config['db_mapping']['distribution']['rel_name'],
-                column(config['db_mapping']['distribution']['id_col']),
-                column(config['db_mapping']['distribution']['entry_id']),
-                column(config['db_mapping']['distribution']['person_id'])
+        distribution = table(db['schema']['distribution']['rel_name'],
+                column(db['schema']['distribution']['id_col']),
+                column(db['schema']['distribution']['entry_id']),
+                column(db['schema']['distribution']['person_id'])
             )
 
-        fact = table(config['db_mapping']['entry']['rel_name'],
-                column(config['db_mapping']['entry']['id_col']),
-                column(config['db_mapping']['entry']['item_desc']),
-                column(config['db_mapping']['entry']['type_id']),
-                column(config['db_mapping']['entry']['vend_id']),
-                column(config['db_mapping']['entry']['amount']),
-                column(config['db_mapping']['entry']['entry_date']),
-                column(config['db_mapping']['entry']['last_updated'])
+        fact = table(db['schema']['entry']['rel_name'],
+                column(db['schema']['entry']['id_col']),
+                column(db['schema']['entry']['item_desc']),
+                column(db['schema']['entry']['type_id']),
+                column(db['schema']['entry']['vend_id']),
+                column(db['schema']['entry']['amount']),
+                column(db['schema']['entry']['entry_date']),
+                column(db['schema']['entry']['last_updated'])
             )
-        
-        #Prefill categories
-        #TODO: Optimize for O(n) time
-        #TODO: Add logic to ignore if categories have already been prefilled
-        for catlabel in db['pre_fill']['category']:
-            sblabel = db['pre_fill']['category'][catlabel]
-            
-            #Build insert for d_category
-            ld.insert_into(engine_url, cat, {'category_desc': catlabel})
-            
-            for i in sblabel:
-                #Build insert for d_subcategory
-                ld.insert_into(engine_url, sbcat, {'subcategory_desc': i})
-                
-                
-                #Build insert for j_type
-                cid = table_to_dict(ld.select_from(engine_url, cat))
-                sbid = table_to_dict(ld.select_from(engine_url, sbcat))
-                
-                j_payload = {'category_id': cid[catlabel], 'subcategory_id': sbid[i]}
-                ld.insert_into(engine_url, j_type, j_payload)
-
-        #Remove variables to reduce scope errors
-        del sblabel 
-        del j_payload
-        del cid
-        del sbid
-
-        #Prefill d_person
-        for fname in db['pre_fill']['person'].keys():
-            ld.insert_into(engine_url, person, {'first_name': fname,
-                                                'last_name': db['pre_fill']['person'][fname]})
 
         #Check and process new vendors from extract
         print('[PENNY] Checking for new vendors...')
@@ -175,7 +153,7 @@ def main():
         for i in in_vendor:
             if i not in cur_vendor:
                 new_vendor.append(
-                    {config['db_mapping']['vendor']['desc_col']: i}
+                    {db['schema']['vendor']['desc_col']: i}
                 )
 
         ld.insert_into(engine_url, vend, new_vendor)
@@ -216,19 +194,19 @@ def main():
             if entry[8] == 1:
                 dist_list.append({
                     "eid": entry[1],
-                    "person_id": person_dict[config['sheet_mapping']['distribution'][0]]
+                    "person_id": person_dict[gs['sheet_mapping']['distribution'][0]]
                 })
                 
             if entry[9] == 1:
                 dist_list.append({
                     "eid": entry[1],
-                    "person_id": person_dict[config['sheet_mapping']['distribution'][1]]
+                    "person_id": person_dict[gs['sheet_mapping']['distribution'][1]]
                 })
                 
             if entry[10] == 1:
                 dist_list.append({
                     "eid": entry[1],
-                    "person_id": person_dict[config['sheet_mapping']['distribution'][2]]
+                    "person_id": person_dict[gs['sheet_mapping']['distribution'][2]]
                 })
             
             
