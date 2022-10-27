490d1a4703df15482a9a0b233fc872437b47b168
Tyler <ttylertran260@gmail.com>
2022-10-27 10:19:20 -0700

    Remove prefill

--- tests/sql/stg_06.sql
+++ tests/sql/stg_06.sql
@@ -2,8 +2,6 @@ DROP TABLE d_person;
 
 CREATE TABLE d_person (
     person_id SERIAL NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30)
 );
-
-
