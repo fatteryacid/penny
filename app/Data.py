43b173faef71a4a050ae2241c4881c76ac2268f0
Tyler <ttylertran260@gmail.com>
2022-11-28 12:00:08 -0800

    Create class to handle extract and load framework

---
+++ app/Data.py
@@ -0,0 +1,168 @@
+# ==================================================
+import pandas as pd
+import uuid
+
+import pickle
+import os
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from sqlalchemy import create_engine
+
+# ==================================================
+# Notes
+'''
+Scope:
+    - extraction from source google sheets
+    - preliminary data transformation for database insertion
+    - load into staging environment
+'''
+
+
+# ==================================================
+class Data():
+    def __init__(self, sheet_id):
+        self.sheet_id = sheet_id
+        self.df = None
+        self.cred = None
+        self.length = None
+        self.initialization = True
+
+    def __repr__(self):
+        return self.df.head(20)
+    
+    @property
+    def size(self):
+        return self.length
+        
+    def auth(self):
+        #Checks for existing tokens
+        if os.path.exists('./secret/token.pickle'):
+            with open('./secret/token.pickle', 'rb') as token:
+                self.cred = pickle.load(token)
+
+
+        #Check for cred validity
+        if not self.cred or not self.cred.valid:
+            if self.cred and self.cred.expired and self.cred.refresh_token:
+                self.cred.refresh(Request())
+
+            else:
+                scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+                flow = InstalledAppFlow.from_client_secrets_file('./secret/client_secrets.json', scopes=scope)
+
+                flow.run_local_server(port=5537, prompt='consent', authorization_prompt_message='')
+
+                self.cred = flow.credentials
+
+            #Writes token for future use
+            with open('./secret/token.pickle', 'wb') as f:
+                pickle.dump(self.cred, f)
+          
+    def fetch(self, desired_range, headers):
+        if not self.cred or self.cred is None:
+            self.auth()
+
+        try:
+            service = build('sheets', 'v4', credentials=self.cred)
+
+            sheet = service.spreadsheets()
+            result = sheet.values().get(
+                spreadsheetId = self.sheet_id,
+                range = desired_range
+            ).execute()
+
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+            return error
+            
+        if 'values' in result.keys():
+            self.df = pd.DataFrame(result['values'], columns=headers)
+            self.length = len(result['values']) - 1
+
+        #Throw exception on no new data for Airflow to catch
+        else:
+            self.done()
+
+    #Prepare data for staging environment
+    def prepare(self):
+        #Process strings
+        for i in self.df.columns:
+            try:
+                self.df[i] = self.df[i].str.lower()
+
+                self.df[i] = self.df[i].str.replace("[@#%^$&*()']", '', regex=True)
+                self.df[i] = self.df[i].str.strip()
+
+
+                self.df[i] = self.df[i].str.replace('[-\s]', '_', regex=True)
+                self.df[i] = self.df[i].str.replace('_{2,}', '_', regex=True)
+
+            except TypeError:
+                pass
+
+        #Process amounts - remove accounting formatting
+        temp = []
+        
+        for i in self.df.itertuples():
+            j = str(i[3])
+            
+            if '(' in j and ')' in j:
+                j = j.replace('(', '') 
+                j = j.replace(')', '')
+                j = '-' + j
+                
+            temp.append(j)
+        
+        self.df['amount'] = temp
+        self.df['amount'] = pd.to_numeric(self.df['amount'])
+
+        #Create UUIDs for each entry
+        self.df['id'] = [uuid.uuid4() for _ in range(len(self.df.index))]
+
+    #Send to db
+    def commit(self, engine_url, rel_name):
+        engine = create_engine(engine_url, echo=False)
+
+        #On first run, replace staging_environment in database
+        if self.initialization:
+            self.df.to_sql(
+                    name = rel_name, 
+                    con=engine,
+                    if_exists='replace',
+                    index=False
+                )
+            self.initialization = False
+
+        #On subsequent runs, add fresh data to staging_environment
+        else:
+            self.df.to_sql(
+                    name = rel_name, 
+                    con=engine,
+                    if_exists='append',
+                    index=False
+                )
+            
+
+        engine.dispose()
+
+    #Get head
+    def peek(self):
+        if self.df is None:
+            return None
+        else:
+            return self.df.head()
+
+    #Get tail  
+    def peer(self):
+        if self.df is None:
+            return None
+        else:
+            return self.df.tail()
+        
+    def done(self):
+        raise Exception('NoDataError')
+    
